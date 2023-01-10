77b6ea0a5cf7ea98777824c402a694451bfe2a08
yogiealfin <yogiealfin23@gmail.com>
2023-01-10 08:07:43 +0700

    Add: Pertemuan 2 PHP dasar

---
+++ pertemuan2/index.php
@@ -0,0 +1,41 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r (Lebih untuk array)
+// var_dump (debugging, menampilkan tipe data dan ukurannya)
+
+// Variable dan Tipe Data
+// Variable
+// $nama = "Yogie";
+// echo "Nama saya adalah $nama";
+
+// Operator
+// Aritmatika => Operator matematika (+ - / * %)
+
+// Concatenation => Penggabungan String (.)
+// $namaDepan = "Yogie";
+// $namaBelakang = "Alfin";
+// echo $namaDepan . " " . $namaBelakang;
+
+// Assignment => (=, +=, -=, /=, *=, %=, .=)
+
+// Perbandingan => (<, >, <=, >=, ==, !=)
+// var_dump(1 == "1");
+// hasilnya akan true
+
+// Identitas => Membandingkan tipe data juga (===, !==)
+// var_dump(1 === "1");
+// Hasilnya akan false karna berbeda tipe data
+
+// Logika => (&& (and), || (or), ! (not))
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+// Hasilnya akan true, hanya butuh salah satu kondisi benar
+
+// var_dump($x < 20 && $x % 2 == 0); 
+// Hasilnya akan false karna satu kondisi tidak benar
+?>
+
