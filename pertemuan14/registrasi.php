1a1492f2ae35b8764c2980204d7603fc2c66f16c
yogiealfin <yogiealfin23@gmail.com>
2023-01-18 21:54:20 +0700

    Add: Pertemuan 14 PHP Dasar (registrasi)

---
+++ pertemuan14/registrasi.php
@@ -0,0 +1,54 @@
+<?php
+require 'functions.php';
+
+if (isset($_POST['submit'])) {
+    if (registrasi($_POST) > 0) {
+        echo "<script>alert('user baru berhasil ditambahkan');</script>";
+    } else {
+        echo mysqli_error($conn);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Registrasi</title>
+    <style>
+        label {
+            display: block;
+        }
+
+        li {
+            list-style: none;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Halaman Registrasi</h1>
+    <form action="" method="post"></form>
+    <ul>
+        <li>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </li>
+        <li>
+            <label for="konfirmasi">Konfirmasi Password</label>
+            <input type="password" name="konfirmasi" id="konfirmasi">
+        </li>
+        <li>
+            <button type="submit" name="submit">Register</button>
+        </li>
+    </ul>
+</body>
+
+</html>
---
+++ pertemuan14/ubah.php
@@ -0,0 +1,81 @@
+<?php
+require "functions.php";
+
+// ambil data pada url
+$id = $_GET["id"];
+// query data games berdasarkan id
+$game = query("SELECT * FROM games WHERE id = $id")[0];
+// cek tombol submit sudah ditekan atau belum
+if (isset($_POST["submit"])) {
+    // Cek adakah data berhasil diubah atau tidak
+    if (ubah($_POST) > 0) {
+        echo "
+        <script>
+        alert('Data berhasil diubah!');
+        document.location.href='index.php';
+        </script>
+        ";
+    } else {
+        echo "
+        <script>
+        alert('Data gagal diubah!');
+        document.location.href='index.php';
+        </script>
+        ";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubah Data Games</title>
+</head>
+
+<body>
+    <h1>Ubah Data Games</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?= $game["id"]; ?>">
+        <input type="hidden" name="coverLama" value="<?= $game["cover"]; ?>">
+        <ul>
+            <li>
+                <label for="name">Judul Game : </label>
+                <input type="text" name="name" id="name" required value="<?= $game["name"]; ?>">
+            </li>
+            <li>
+                <label for=" developer">Developer : </label>
+                <input type="text" name="developer" id="developer" required value="<?= $game["developer"]; ?>">
+            </li>
+            <li>
+                <label for="publisher">Publisher : </label>
+                <input type="text" name="publisher" id="publisher" required value="<?= $game["publisher"]; ?>">
+            </li>
+            <li>
+                <label for=" genre">Genre : </label>
+                <input type="text" name="genre" id="genre" required value="<?= $game["genre"]; ?>">
+            </li>
+            <li>
+                <label for=" platform">Platform : </label>
+                <input type="text" name="platform" id="platform" required value="<?= $game["platform"]; ?>">
+            </li>
+            <li>
+                <label for=" release_date">Tahun Release : </label>
+                <input type="date" name="release_date" id="release_date" required value="<?= $game["release_date"]; ?>">
+            </li>
+            <li>
+                <label for=" cover">Cover : </label><br>
+                <img src="images/<?= $game['cover']; ?>" alt="" width="150"><br>
+                <input type="file" name="cover" id="cover" required">
+            </li>
+            <li>
+                <button type=" submit" name="submit">Ubah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
